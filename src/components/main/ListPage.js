d4185f8b9d873be59abe7aaee5c0af135e6d5aff
jihyeon <ajjh1025@gmail.com>
2023-05-15 17:47:55 +0900

    axios error

--- src/components/main/ListPage.js
+++ src/components/main/ListPage.js
@@ -1,9 +1,27 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const ListPage = ({ category }) => {
   useEffect(() => {
-    // console.log(category);
+    getData();
+    console.log(category);
   }, [category]);
+
+  //받아오는 부분 여기
+  const getData = async () => {
+    const param = {
+      data: { category: category },
+    };
+    try {
+      const res = await axios.post(
+        `http://localhost:8080/clothes/${category}`,
+        param
+      );
+      console.log("Success");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return <div></div>;
 };
