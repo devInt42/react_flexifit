551a13c819891a1a5f82d1619328619607b2f605
jihyeon <ajjh1025@gmail.com>
2023-05-21 13:16:02 +0900

    sort

--- src/pages/SweatShirtListPage.js
+++ src/pages/SweatShirtListPage.js
@@ -14,7 +14,6 @@ const SweatShirtListPage = () => {
     getCount();
   }, [count]);
 
-  //count
   const getCount = async (category) => {
     try {
       const res = await axios.get("http://localhost:8080/clothes/count", {
--- src/pages/TshirtListPage.js
+++ src/pages/TshirtListPage.js
@@ -14,7 +14,6 @@ const TshirtPage = () => {
     getCount();
   }, [count]);
 
-  //count
   const getCount = async (category) => {
     try {
       const res = await axios.get("http://localhost:8080/clothes/count", {
