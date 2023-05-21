b7c932841409d7607fd6cb7a1ea0b087137a2f08
jihyeon <ajjh1025@gmail.com>
2023-05-21 13:15:24 +0900

    add selectBox

--- src/pages/AllListsPage.js
+++ src/pages/AllListsPage.js
@@ -14,7 +14,6 @@ const AllListPage = () => {
     getCount();
   }, [count]);
 
-  //count
   const getCount = async (category) => {
     try {
       const res = await axios.get("http://localhost:8080/clothes/count", {
