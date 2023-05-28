858ce5da20108da09c67d141ffe4e6feddfabbe9
jihyeon <ajjh1025@gmail.com>
2023-05-28 19:32:10 +0900

    faq

--- src/pages/FAQPage.js
+++ src/pages/FAQPage.js
@@ -1,27 +1,58 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "../styles/pages/FAQ.css";
 import { getFAQData } from "../store/action";
 
 const FAQPage = () => {
   const dispatch = useDispatch();
   const faqList = useSelector((state) => state.faqData);
+  const [selectedTitle, setSelectedTitle] = useState("");
+  const [selectedContent, setSelectedContent] = useState("");
+  const isLoading = useSelector((state) => state.isLoading);
 
   useEffect(() => {
     dispatch(getFAQData());
   }, [dispatch]);
 
+  const handleTitleClick = (title, content) => {
+    if (selectedTitle === title) {
+      setSelectedTitle("");
+      setSelectedContent("");
+    } else {
+      setSelectedTitle(title);
+      setSelectedContent(content);
+    }
+  };
+
   return (
-    <div className="qna-page">
-      {faqList.map((faq) => (
-        <div key={faq.faq_id} className="qna-item">
-          <div className="qna-item-content">
-            <div>
-              <span className="qna-item-title">{faq.faq_title}</span>
-              <span className="qna-item-author">작성자:</span>
-            </div>
-          </div>
+    <div className="faq-page">
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          <span className="faqLogo">FAQ</span>
+          <span className="faqminiLogo">주문 / 결제</span>
+          <hr />
+          {faqList &&
+            faqList.map((faq) => (
+              <div key={faq.faq_id}>
+                <div
+                  className={`faq-title ${
+                    selectedTitle === faq.faq_title ? "selected" : ""
+                  }`}
+                  onClick={() =>
+                    handleTitleClick(faq.faq_title, faq.faq_content)
+                  }
+                >
+                  {faq.faq_title}
+                </div>
+                {selectedTitle === faq.faq_title && (
+                  <div className="faq-content">{faq.faq_content}</div>
+                )}
+              </div>
+            ))}
         </div>
-      ))}
+      )}
     </div>
   );
 };
