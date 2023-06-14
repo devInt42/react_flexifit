82eac796245d4f286a0925ed65c4a2ecc54f2666
jihyeon <ajjh1025@gmail.com>
2023-06-14 17:21:13 +0900

    css

--- src/pages/ReviewPage.js
+++ src/pages/ReviewPage.js
@@ -29,33 +29,60 @@ const ReviewPage = () => {
   };
 
   return (
-    <div className="review-container">
-      <div className="review-Logo">내 정보</div>
-      <div className="review-title">주문 상품 정보 </div>
+    <div className="review-titleContainer">
+      <div className="review-container">
+        <div className="review-Logo">내 정보</div>
+        <div className="review-title">주문 상품 정보 </div>
 
-      <div className="review-list">
-        <div className="review-date">주문일자</div>
-        <div className="review-image">이미지</div>
-        <div className="review-info">상품정보</div>
-        <div className="review-count">수량</div>
-        <div className="review-price">상품구매금액</div>
-        <div className="review-status">주문처리상태</div>
+        <div className="review-list">
+          <div className="review-date">주문일자</div>
+          <div className="review-image">이미지</div>
+          <div className="review-info">상품정보/수량</div>
+
+          <div className="review-price">상품구매금액</div>
+          <div className="review-status">주문처리상태</div>
+        </div>
+        {myReview.length > 0 ? (
+          myReview.map((review) => (
+            <div key={review.cloth_id} className="review-item">
+              <div className="review-cloth-date">{review.cloth_date}</div>
+              <div className="review-cloth-image">
+                <img
+                  className="review-cloth-defaultImage"
+                  src={review.cloth_defaultImage}
+                  alt="Cloth Image"
+                />
+              </div>
+              <div className="review-cloth-info">
+                <div className="review-cloth-name">
+                  {review.cloth_name} / {review.cloth_totalCount}
+                </div>
+                <div className="review-cloth-color">
+                  [옵션 : {review.cloth_color}]
+                </div>
+              </div>
+              <div className="review-cloth-discount">
+                {review.cloth_discount}원
+              </div>
+              <div className="review-cloth-deliver">
+                배송완료
+                <div className="review-cloth-button">
+                  <button
+                    type="button"
+                    class="btn btn-dark small-button"
+                    style={{ marginTop: "10px", width: "75px" }}
+                  >
+                    구매후기
+                  </button>
+                </div>
+              </div>
+              <div className="border-line" />
+            </div>
+          ))
+        ) : (
+          <div>작성 가능한 리뷰가 없습니다.</div>
+        )}
       </div>
-      {myReview.length > 0 ? (
-        myReview.map((review) => (
-          <div key={review.cloth_id}>
-            <div>{review.cloth_date}</div>
-            <img src={review.cloth_defaultImage} alt="Cloth Image" />
-            <div>{review.cloth_name}</div>
-            <div>{review.cloth_totalCount}</div>
-            <div>{review.cloth_color}</div>
-            <div>{review.cloth_discount}</div>
-            <div>배송완료 </div>
-          </div>
-        ))
-      ) : (
-        <div>작성 가능한 리뷰가 없습니다.</div>
-      )}
     </div>
   );
 };
